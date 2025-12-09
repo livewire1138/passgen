eeb381ecbcdb696f024690a792bd8d46a99d3d30
livewire1138 <helkerjacob@icloud.com>
2025-12-08 21:32:41 -0600

    Added password choice capability

--- passgen_main.py
+++ passgen_main.py
@@ -6,13 +6,20 @@ DIGIT_LIBRARY = '1234567890'
 SYMBOL_LIBRARY = '-/:;$&@.,?!'
 
 #Combined library of possible characters
-combined_library = LETTER_LIBRARY + DIGIT_LIBRARY + SYMBOL_LIBRARY
+combined_library = LETTER_LIBRARY + DIGIT_LIBRARY
+
+length = int(input('How many characters should be in your password?'))
 
 #password init
 password = ""
 
+symbols_true = input('Should your password use special characters? [Y] or [N]').upper()
+
+if symbols_true == 'Y' or symbols_true == 'YES':
+    combined_library += SYMBOL_LIBRARY
+
 #inital for loop to generate
-for i in range(8):
+for i in range(length):
     character = random.choice(combined_library)
     password += character
 
